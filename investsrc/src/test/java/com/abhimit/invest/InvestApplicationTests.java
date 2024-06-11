0269e43a48d83f45e323e6d38089c22e7694ea83
armaster <armaster@cisco.com>
2024-06-11 10:34:19 +0530

    first checkin

---
+++ investsrc/src/test/java/com/abhimit/invest/InvestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.abhimit.invest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//TODO to be implmented
+	}
+
+}
